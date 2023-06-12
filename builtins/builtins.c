b6d8da9f2ec64cda4df7a1a585c34a57ec95cbd6
Mouad Sodor <sodormouad@gmail.com>
2023-06-12 17:38:40 +0100

    echo done

--- builtins/builtins.c
+++ builtins/builtins.c
@@ -6,7 +6,7 @@
 /*   By: msodor <msodor@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 14:30:12 by msodor            #+#    #+#             */
-/*   Updated: 2023/06/12 10:31:13 by msodor           ###   ########.fr       */
+/*   Updated: 2023/06/12 17:29:08 by msodor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 
 void	builtins(t_cmd *cmds)
 {
-	if (ft_strncmp(cmds->cmd, "echo", ft_strlen(cmds->cmd)) == 0)
-		ft_echo(cmds);
+	if (cmds->cmd[0])
+	{
+		if (ft_strncmp(cmds->cmd, "echo", ft_strlen(cmds->cmd)) == 0)
+			ft_echo(cmds);
+	}
+
 }
